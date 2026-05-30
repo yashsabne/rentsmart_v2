4edf35fd6858c4962efea86b829147e978db37ba
Yash Sabne <yashsabne2006@gmail.com>
2026-05-30 11:03:25 +0530

    changed port mail

--- services/auth-service/src/config/mail.js
+++ services/auth-service/src/config/mail.js
@@ -1,13 +1,12 @@
 import nodemailer from "nodemailer";
 
-throw new Error("MAIL_JS_TEST");
-
+ 
  console.log("MAIL.JS LOADED MAIL.JS LOADED MAIL.JS LOADED MAIL.JS LOADED MAIL.JS LOADEDMAIL.JS LOADEDMAIL.JS LOADEDMAIL.JS LOADEDMAIL.JS LOADEDMAIL.JS LOADED ");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
