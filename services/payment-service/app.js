a9a5e6cab9d8ee7a925488620da1cc9c88f9c168
Yash Sabne <yashsabne2006@gmail.com>
2026-06-09 23:21:54 +0530

    payment service

--- services/payment-service/app.js
+++ services/payment-service/app.js
@@ -6,6 +6,7 @@ import cors from "cors";
 
 import connectDB from "./src/config/db.js";
 import paymentRoutes from "./src/routes/paymentRoutes.js";
+import paymentHistoryRoutes from "./src/routes/historyRoutes.js"
 
 dotenv.config();
 
@@ -23,10 +24,10 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use(
-  "/api/payment",
-  paymentRoutes
-);
+app.use("/api/payment",paymentRoutes);
+
+app.use("/api/payments", paymentHistoryRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Payment Service Running");
