3b3f53cb8abfcb0062dbedeee1d290a7d3b7a0b2
Yash Sabne <yashsabne2006@gmail.com>
2026-06-21 11:49:36 +0530

    auith midleware

--- services/property-service/src/middleware/authMiddleware.js
+++ services/property-service/src/middleware/authMiddleware.js
@@ -4,16 +4,11 @@ import jwt from "jsonwebtoken";
 const authMiddleware = (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
-
- 
     if (!token) {
       return res.status(401).json({ message: "No token" });
     }
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
- 
     req.user = decoded;
-
     next();
   } catch (err) {
     console.log("error",err )
