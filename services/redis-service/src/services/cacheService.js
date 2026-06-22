d05330723790e1726cb8c34fd4444d5f6cbb474e
Yash Sabne <yashsabne2006@gmail.com>
2026-06-22 17:57:03 +0530

    redis logs

--- services/redis-service/src/services/cacheService.js
+++ services/redis-service/src/services/cacheService.js
@@ -5,12 +5,18 @@ const DEFAULT_TTL = parseInt(process.env.CACHE_TTL) || 3600;
 
 export const setCache = async (identifier, data, ttl = DEFAULT_TTL) => {
   const client = getRedisClient();
+
+    console.log("Redis status:", client?.status);
+
+
   const key = keys.cache(identifier);
   await client.set(key, JSON.stringify(data), "EX", ttl);
 };
 
 export const getCache = async (identifier) => {
   const client = getRedisClient();
+    console.log("Redis status:", client?.status);
+
   const key = keys.cache(identifier);
   const data = await client.get(key);
   return data ? JSON.parse(data) : null;
@@ -18,12 +24,16 @@ export const getCache = async (identifier) => {
 
 export const deleteCache = async (identifier) => {
   const client = getRedisClient();
+    console.log("Redis status:", client?.status);
+
   const key = keys.cache(identifier);
   await client.del(key);
 };
 
 export const flushCacheByPattern = async (pattern) => {
   const client = getRedisClient();
+    console.log("Redis status:", client?.status);
+
   const matchPattern = keys.cache(pattern);
   const matchedKeys = await client.keys(matchPattern);
   if (matchedKeys.length > 0) {
