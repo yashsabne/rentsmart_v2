4273a99fefdd5245739ae926715838162da36816
Yash Sabne <yashsabne2006@gmail.com>
2026-06-28 11:01:41 +0530

    generateToken.js removed phone

--- services/auth-service/src/utils/generateToken.js
+++ services/auth-service/src/utils/generateToken.js
@@ -6,7 +6,6 @@ export const generateToken = (user) => jwt.sign(
     id: user._id,
     city: user.city || null,
     preferences: user.preferences || [], 
-    phone: user.phone || null,                         
   },
   process.env.JWT_SECRET,
   { expiresIn: "5d" }
